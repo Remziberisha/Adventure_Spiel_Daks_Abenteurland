27c9b46cd55bcd30434a83086427f324dd75a886
Remziberisha <Remzi.berisha@training.lug-ag.de>
2025-12-08 01:16:21 +0100

    Projekt zu .NET migriert für JSON-Serialisierung

---
+++ Adventure_spiel_RemziBerisha/Program.cs
@@ -0,0 +1,13 @@
+﻿
+
+namespace Adventure_Spiel_RemziBerisha
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Spiel spiel = new Spiel();
+            spiel.Start();
+        }
+    }
+}
